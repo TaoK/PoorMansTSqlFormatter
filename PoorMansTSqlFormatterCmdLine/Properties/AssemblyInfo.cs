3de445a1d519f988c430fe217d41a69cbabf28df
Dmitry Afanasiev <afanasiev.dmitry@gmail.com>
2015-02-21 20:56:32 +0300

    Auto detect input file encoding using Mozilla Universal Charset Detector (C# port from https://code.google.com/p/ude/).

--- PoorMansTSqlFormatterCmdLine/Properties/AssemblyInfo.cs
+++ PoorMansTSqlFormatterCmdLine/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4dacf3ba-d535-4244-b756-7d9ecfe577ae")]
-[assembly: AssemblyVersion("1.5.1.*")]
+[assembly: AssemblyVersion("1.5.2.*")]
